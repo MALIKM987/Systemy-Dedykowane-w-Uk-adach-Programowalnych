0e615a88b6dbed46983e319fc932eb9ed03cdb30
Maciej Molik <135590265+MALIKM987@users.noreply.github.com>
2026-03-17 22:44:28 +0100

    Create pwm.v

---
+++ Laboratorium 0/Laboratorium 0.srcs/sources_1/new/pwm.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.03.2026 17:07:27
+// Design Name: 
+// Module Name: pwm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm(
+
+    );
+endmodule
